06521f6998c7077ea4b63cae96c17f7ad799af33
Theo <theo.le.wiik@gmail.com>
2022-01-26 23:16:46 +0100

    Add holding ability

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -2,9 +2,22 @@ using Godot;
 
 public sealed class Main : Node
 {
+    private Camera _playerCamera;
+    private RigidBody _pickup;
+
     public override void _Ready()
     {
+        _pickup = GetNode<RigidBody>("Holding");
+        _playerCamera = GetNode<Camera>("Player/CameraPivot/Camera");
         Input.SetMouseMode(Input.MouseMode.Captured);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        var desiredPos = _playerCamera.GlobalTransform.origin - _playerCamera.GlobalTransform.basis.z * 5;
+        var currentPos = _pickup.GlobalTransform.origin;
+
+        _pickup.ApplyCentralImpulse(desiredPos - currentPos);
     }
 
     public override void _UnhandledInput(InputEvent @event)
