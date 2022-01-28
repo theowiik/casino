6b553dd451af3ce2c538c4850ab23c15ecbcf9ba
Theo <theo.le.wiik@gmail.com>
2022-01-28 20:33:28 +0100

    Add floating text

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -6,12 +6,21 @@ public sealed class Main : Node
     private RigidBody _pickup;
     private float _timer;
     private const float _forceTime = 0.01f;
+    // private Label _label;
 
     public override void _Ready()
     {
+        // _label = GetNode<Label>("Sprite3D/Label");
         _pickup = GetNode<RigidBody>("Holding");
         _playerCamera = GetNode<Camera>("Player/CameraPivot/Camera");
         Input.SetMouseMode(Input.MouseMode.Captured);
+
+        // PositionLabel();
+
+        var viewport = GetNode<Viewport>("Wasd/Viewport");
+        var sprite3d = GetNode<Sprite3D>("Wasd");
+
+        sprite3d.Texture = viewport.GetTexture();
     }
 
     public override void _Process(float delta)
@@ -21,8 +30,7 @@ public sealed class Main : Node
         if (_timer > _forceTime)
         {
             _timer = 0f;
-            GD.Print("Force");
-            Hold();
+            // Hold();
         }
     }
 
@@ -40,8 +48,36 @@ public sealed class Main : Node
 
     public override void _UnhandledInput(InputEvent @event)
     {
-        if (@event.IsActionPressed("ui_cancel")) {
+        if (@event.IsActionPressed("ui_cancel"))
+        {
             GetTree().Quit();
         }
     }
+
+    private Camera GetCameraViewport()
+    {
+        var camera = GetNode<Camera>("Player/CameraPivot/Camera");
+        return camera.GetViewport().GetCamera();
+    }
+
+    // private void PositionLabel()
+    // {
+    //     var viewport = GetCameraViewport();
+    //     var camPos = viewport.GlobalTransform.origin;
+    //     var offset = new Vector2(_label.RectSize.x / 2, 0);
+    //     _label.RectPosition = viewport.UnprojectPosition(new Vector3(0, 3, 0)) - offset;
+    // }
+
+    // onready var sprite = $Sprite3D
+    // onready var label = $Sprite3D/Label
+
+    // func get_camera():
+    //     var r = get_node('/root')
+    //     return r.get_viewport().get_camera()
+
+    // func position_label(label:Label, point3D:Vector3):
+    //     var camera = get_camera()
+    //     var cam_pos = camera.translation
+    //     var offset = Vector2(label.get_size().x/2, 0)
+    //     label.rect_position = camera.unproject_position(point3D) - offset
 }
