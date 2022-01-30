6bdf88956533d272266484447ff457c7d93ebe3b
Theo <theo.le.wiik@gmail.com>
2022-01-30 20:09:26 +0100

    Fix spawning of cards

--- Scripts/CardFan.cs
+++ Scripts/CardFan.cs
@@ -5,7 +5,7 @@ public sealed class CardFan : Spatial
 {
     private IList<PhysicalCard> _cards;
     private PackedScene _cardScene;
-    private const float Radius = 1f;
+    private const float Radius = 3f;
     private const float PercentageOfCircle = 0.5f;
     private const float ZOffset = 0.1f;
 
@@ -23,24 +23,27 @@ public sealed class CardFan : Spatial
     private void Place()
     {
         var i = 0;
-        var basis = Transform.basis;
         var angleBetweenCardsRad = PercentageOfCircle * Mathf.Tau / _cards.Count;
         var totalRad = angleBetweenCardsRad * _cards.Count;
-        var baseVector = basis.y.Normalized() * Radius;
+        var baseVector = new Vector3(0, Radius, 0);
 
         foreach (var card in _cards)
         {
-            var forward = basis.z.Normalized();
-            var rotated = baseVector.Rotated(forward, i * angleBetweenCardsRad);
-            card.Transform = new Transform(basis, rotated + basis.z.Normalized() * ZOffset);
-            //card.RotateZ(-i * angleBetweenCardsRad);
+            var rotated = baseVector.Rotated(Vector3.Forward, i * angleBetweenCardsRad);
+            card.Transform = new Transform(card.Transform.basis, rotated + new Vector3(0, 0, ZOffset));
+            card.RotateZ(-i * angleBetweenCardsRad);
             i++;
         }
     }
 
-    public void Add(Card card)
+    public void Add(Card cardModel)
     {
-        BuildAndAdd(card);
+        var card = _cardScene.Instance<PhysicalCard>();
+        card.SetAsStatic();
+        AddChild(card);
+        card.Text = cardModel.Name;
+        _cards.Add(card);
+
         Place();
     }
 
@@ -50,28 +53,5 @@ public sealed class CardFan : Spatial
             card.QueueFree();
 
         _cards.Clear();
-    }
-
-    public override void _PhysicsProcess(float delta)
-    {
-        RotateY(delta * Mathf.Tau / 10);
-
-        if (Input.IsActionJustPressed("ui_right"))
-        {
-            Clear();
-            for (int i = 0; i < 1; i++)
-            {
-                Add(new Card(21, "test card", Suit.Clubs));
-            }
-        }
-    }
-
-    private void BuildAndAdd(Card cardModel)
-    {
-        var card = _cardScene.Instance<PhysicalCard>();
-        card.SetAsStatic();
-        AddChild(card);
-        card.Text = cardModel.Name;
-        _cards.Add(card);
     }
 }
